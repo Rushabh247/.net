5993f5235354f333c6fe584ab49bf7f8e4e3fb8d
Rushabh Gunjal <78420779+Rushabh247@users.noreply.github.com>
2025-07-02 09:06:23 -0700

    Add files via upload

---
+++ MoviestoreApp/Movie.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieStoreApp
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int YearOfRelease { get; set; }
+        public string Genre { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Name: {Name}, Year: {YearOfRelease}, Genre: {Genre}";
+        }
+
+        
+        private static List<Movie> movieList = new List<Movie>();
+        private const int MaxMovies = 5;
+
+        
+        public static void AddMovie()
+        {
+            if (movieList.Count >= MaxMovies)
+            {
+                Console.WriteLine("Cannot add more than 5 movies.");
+                return;
+            }
+
+            Movie movie = new Movie();
+
+            Console.Write("Enter Movie ID: ");
+            movie.Id = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Movie Name: ");
+            movie.Name = Console.ReadLine();
+
+            Console.Write("Enter Year of Release: ");
+            movie.YearOfRelease = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Genre: ");
+            movie.Genre = Console.ReadLine();
+
+            movieList.Add(movie);
+            Console.WriteLine("Movie added successfully.");
+        }
+
+        
+        public static void DisplayAllMovies()
+        {
+            if (movieList.Count == 0)
+            {
+                Console.WriteLine("No movies available.");
+                return;
+            }
+
+            Console.WriteLine("All Movies:");
+            foreach (var movie in movieList)
+            {
+                Console.WriteLine(movie);
+            }
+        }
+
+      
+        public static void FindMovieById()
+        {
+            Console.Write("Enter Movie ID to search: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Movie foundMovie = null;
+
+            foreach (Movie m in movieList)
+            {
+                if (m.Id == id)
+                {
+                    foundMovie = m;
+                    break;
+                }
+            }
+
+            if (foundMovie != null)
+            {
+                Console.WriteLine("Movie Found:");
+                Console.WriteLine(foundMovie);
+            }
+            else
+            {
+                Console.WriteLine("Movie not found.");
+            }
+        }
+
+
+        
+        public static void RemoveMovieById()
+        {
+            Console.Write("Enter Movie ID to remove: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Movie movieToRemove = null;
+
+            foreach (Movie m in movieList)
+            {
+                if (m.Id == id)
+                {
+                    movieToRemove = m;
+                    break;
+                }
+            }
+
+            if (movieToRemove != null)
+            {
+                movieList.Remove(movieToRemove);
+                Console.WriteLine("Movie removed successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Movie not found.");
+            }
+        }
+
+
+        
+        public static void ClearAllMovies()
+        {
+            movieList.Clear();
+            Console.WriteLine("All movies cleared.");
+        }
+    }
+}
