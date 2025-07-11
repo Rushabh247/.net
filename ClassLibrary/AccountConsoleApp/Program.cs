b33116aaa34fb7cebf67ec04ef295f305b50ad9f
Rushabh Gunjal <78420779+Rushabh247@users.noreply.github.com>
2025-07-11 02:07:36 -0700

    Add files via upload

---
+++ ClassLibrary/AccountConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using AccountClassLib;
+namespace AccountConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Account ac = new Account("ICICI2424","Rushabh",7000);
+            ac.Deposit(5000);
+            ac.Deposit(3000);
+            ac.Withdraw(2000);
+           Console.WriteLine(ac.ToString());
+
+        }
+    }
+}
