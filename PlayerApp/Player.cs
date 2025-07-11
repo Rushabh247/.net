156788e296b38a7aa6d459d5c1cdadb096c18498
Rushabh Gunjal <78420779+Rushabh247@users.noreply.github.com>
2025-07-11 03:23:38 -0700

    Add files via upload

---
+++ PlayerApp/Player.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerApp
+{
+    internal class Player
+    {
+        public readonly int id;
+        public readonly string name;
+        public readonly int age;
+        public Player(int id, string name, int age)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+        }
+        public Player(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = 10;
+            
+        }
+        public int getId()
+        {
+            return id;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public int getAge()
+        {
+            return age;
+        }
+
+        public Player WhoIsElder(Player player)
+        {
+            if (player.age > this.age)
+                return player;
+            else
+                return this;
+        }
+
+    }
+}
