ff52ef3035a329bed7775ff0274b550cb85dd518
Rushabh Gunjal <78420779+Rushabh247@users.noreply.github.com>
2025-07-11 02:04:27 -0700

    Add files via upload

---
+++ ClassLibrary/AccountsClassLibrary/Account.cs
@@ -0,0 +1,42 @@
+﻿namespace AccountClassLib
+{
+    
+        public class Account
+        {
+            public string AccountNumber { get; set; }
+            public string AccountHolder { get; set; }
+            public decimal Balance { get; private set; }
+
+            public Account(string accountNumber, string accountHolder, decimal initialBalance)
+            {
+                AccountNumber = accountNumber;
+                AccountHolder = accountHolder;
+                Balance = initialBalance;
+            }
+
+            public void Deposit(decimal amount)
+            {
+                if (amount > 0)
+                {
+                    Balance += amount;
+                }
+            }
+
+            public bool Withdraw(decimal amount)
+            {
+                if (amount > 0 && amount <= Balance)
+                {
+                    Balance -= amount;
+                    return true;
+                }
+                return false;
+            }
+
+            public override string ToString()
+            {
+                return $"Account: {AccountNumber}, Holder: {AccountHolder}, Balance: {Balance:C}";
+            }
+        }
+    }
+
+
